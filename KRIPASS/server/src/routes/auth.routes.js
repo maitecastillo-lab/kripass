4571987eedd0c3424af79a60d0c1e1f487fd5273
Maite Castillo <maitecastillo2403@gmail.com>
2026-04-23 20:33:43 +0200

    mayus

--- KRIPASS/server/src/routes/auth.routes.js
+++ KRIPASS/server/src/routes/auth.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const crypto = require('crypto');
-const User = require('../models/user');
+const User = require('../models/User');
 
 // Hash simple con SHA-256 (para un proyecto académico es suficiente)
 const hashPassword = (password) => {
